a0e57936dec4604cb5f857aab075d0ba9bb3e12d
theo <theo.middleton@outlook.com>
2023-07-07 11:26:12 +0100

    changed for gae

--- next.config.js
+++ next.config.js
@@ -3,10 +3,5 @@ module.exports = {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com', "s0.darkroom.com"],
   },
+  distDir: 'build',
 }
-
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-+ output: "standalone",
-});
